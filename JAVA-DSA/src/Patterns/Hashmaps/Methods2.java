087c4288191ccecdd9e073b0efd1a72908a1b2b9
veeresh <pasareveeresh0908@gmail.com>
2026-04-27 15:19:47 +0530

    started hashmap pattern

--- JAVA-DSA/src/Patterns/Hashmaps/Methods2.java
+++ JAVA-DSA/src/Patterns/Hashmaps/Methods2.java
@@ -1,7 +1,30 @@
 package Patterns.Hashmaps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Methods2 {
     public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Apple" , 1);
+        map.put("Banana" , 2);
+        map.put("Veeru" , 3);
+        map.put("Surag" , 4);
+        map.put("Pajju" , 1);
 
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + "->" + entry.getValue());
+        }
+
+        for(String key : map.keySet()){
+            System.out.println(key + "->" + map.get(key));
+        }
+        for(Integer value : map.values()){
+            System.out.println(value);
+        }
+
+        map.forEach((key,value)->{
+            System.out.println(key + "->" + value);
+        });
     }
 }
